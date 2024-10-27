554c36aa597e44a62ce04c8a9f1c115b990ba151
Mehrdad Ahmadi <mehrdad2maskull@gmail.com>
2024-10-27 15:00:16 -0400

    Initial Commit

---
+++ tg-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tg-test
+//
+//  Created by Mehrdad Ahmadi on 2024-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
